9f659a56b5cf4aa6fc61330f8792b7fbb9b54f31
jason <jasontan9178@gmail.com>
2023-01-21 23:08:31 +0800

    first commit

---
+++ pipeline1/pipeline.py
@@ -0,0 +1,21 @@
+# Creation Date: 20/1/2023
+# Author: Jason
+# Topic: Docker Practice
+
+import pandas as pd
+
+# carry out task with pandas
+
+print('job finished successfully, hurray!')
+
+
+# Follow following dockerfile
+
+# FROM python:3.9.1 
+
+# RUN pip install pandas
+
+# WORKDIR /app
+# COPY pipeline.py pipeline.py
+
+# ENTRYPOINT [ "bash" ]
---
+++ pipeline2/pipeline2.py
@@ -0,0 +1,15 @@
+# Creation Date: 20/1/2023
+# Author: Jason
+# Topic: Docker Practice
+
+import pandas as pd
+
+import sys
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+# carry out task with pandas
+
+print(f'job finished successfully for day = {day}, hurray!')
